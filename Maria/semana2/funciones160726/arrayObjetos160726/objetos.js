d89b964c88b1c841909fc4fd76c363c7237e7716
Tavo <gjuarez25@politecnics.barcelona>
2026-07-17 17:31:32 +0200

    viernes

--- Maria/semana2/funciones160726/arrayObjetos160726/objetos.js
+++ Maria/semana2/funciones160726/arrayObjetos160726/objetos.js
@@ -5,7 +5,7 @@ let planes = [
     nombre: "Concierto al aire libre",
     lugar: "playa de bogatell",
     precio: "10",
-    categorita: "Musica",
+    categoria: "Musica",
     icono: "🪩",
     imagen: "img/concert.jpg",
     reservado: false,
@@ -14,7 +14,7 @@ let planes = [
     nombre: "Visita expo",
     lugar: "MACBA",
     precio: 0,
-    categorita: "Arte",
+    categoria: "Arte",
     icono: "🫠",
     imagen: "img/museo.jpg",
     reservado: true,
@@ -23,7 +23,7 @@ let planes = [
     nombre: "Rafting",
     lugar: "Riu Vinalopó",
     precio: 22,
-    categorita: "Deporte",
+    categoria: "Deporte",
     icono: "🛶",
     imagen: "img/rafting.jpg",
     reservado: false,
@@ -32,7 +32,7 @@ let planes = [
     nombre: "Drag Show",
     lugar: "Strass",
     precio: 10,
-    categorita: "Drag",
+    categoria: "Drag",
     icono: "👑",
     imagen: "img/drag.jpg",
     reservado: false,
@@ -42,32 +42,76 @@ let planes = [
 
 const listaPlanes = document.getElementById("listaPlanes");
 
-for (let plan of planes) {
-  let mensajeEstado;
-  let claseEstado;
-  if (plan.reservado === true) {
-    mensajeEstado = "Ya está reservado";
-    claseEstado = "reservado";
-  } else {
-    mensajeEstado = "Plan disponible";
-    claseEstado = "disponible";
-  }
+function mostrarPlanes() {
+  listaPlanes.innerHTML = "";
 
-  // el += añade el elemento a la lista sin omitir el anterior
-  listaPlanes.innerHTML += `
+  for (let i = 0; i < planes.length; i++) {
+    // for (let plan of planes)
+    let plan = planes[i]; //necesito esta variable al haber cambiado la definicion de bucle
+    let mensajeEstado;
+    let claseEstado;
+    if (plan.reservado === true) {
+      mensajeEstado = "Ya está reservado";
+      claseEstado = "reservado";
+    } else {
+      mensajeEstado = "Plan disponible";
+      claseEstado = "disponible";
+    }
+
+    //Poner "Gratis" cuando el precio es 0
+    let textoPrecio;
+    if (plan.precio === 0) {
+      textoPrecio = "Gratis";
+    } else {
+      textoPrecio = `${plan.precio} €`;
+    }
+
+    // el += añade el elemento a la lista sin omitir el anterior
+    listaPlanes.innerHTML += `
    <article class='tarjeta'>
    <div class='contenedorImagen'>
    <img src=${plan.imagen} alt='${plan.nombre}'>
    
    <span class='icono'>${plan.icono}</span>
    </div>
+
    <div class='contenidoTarjeta'>
-   <p class='categoria'>${plan.categoría}</p>
+   <p class='categoria'>${plan.categoria}</p>
    <h2>${plan.nombre} ${plan.icono}</h2>
    <p>Lugar: ${plan.lugar}</p>
-   <p>Precio: ${plan.precio}</p>
-   <p>${mensajeEstado}</p>
+   <p>Precio: ${textoPrecio}</p>
+   <p class='estado ${claseEstado}' >${mensajeEstado}</p>
    </div>
+   <button class='btnReservar' data-indice='${i}'>Cambiar reserva</button> 
    </article>
    `;
+  }
+  let botonesReservar = document.querySelectorAll(".btnReservar");
+
+  for (let boton of botonesReservar) {
+    boton.addEventListener("click", cambiarReserva)
+    }
+
+
+    function cambiarReserva(ev){
+      // target.dataset identificamos nuestro atributo personalizado
+let indice = ev.target.dataset.indice // indice es mi atributo persolalizado
+//console.log(indice)
+planes[indice].reservado = !planes[indice].resrvado
+mostrarPlanes()
+    }
 }
+
+
+// "Push"   agrega un nuevo elemento en el array de planes en ultima posicion
+planes.push({
+  nombre: "Beer Pong",
+  lugar: "Playa",
+  precio: "Lo que cueste",
+  categoria: "Deporte",
+  icono: "🍺",
+  imagen: "img/beerPong.jpg",
+  reservado: true,
+});
+
+mostrarPlanes();
